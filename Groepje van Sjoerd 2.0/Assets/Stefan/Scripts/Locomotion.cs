291d3271a576b66d64557716d473921d6e12ea0f
StefanHagewoud <stefanhagewoud@gmail.com>
2022-05-15 16:31:24 +0200

    teleportation system jippie

--- Groepje van Sjoerd 2.0/Assets/Stefan/Scripts/Locomotion.cs
+++ Groepje van Sjoerd 2.0/Assets/Stefan/Scripts/Locomotion.cs
@@ -8,6 +8,7 @@ public class Locomotion : MonoBehaviour
 {
     // Start is called before the first frame update
     public OVRPlayerController ovrRig;
+    public GameObject teleportEmpty;
     public TextMeshProUGUI textMesh;
     public bool RotationBool = true;
     public bool LocomotionBool = true;
@@ -27,12 +28,14 @@ public class Locomotion : MonoBehaviour
         if (LocomotionBool == true)
         {
             ovrRig.GetComponent<OVRPlayerController>().EnableLinearMovement = false;
+            teleportEmpty.SetActive(true);
             textMesh.text = "TELEPORTATION";
             LocomotionBool = false;
         }
         else if (LocomotionBool == false)
         {
             ovrRig.GetComponent<OVRPlayerController>().EnableLinearMovement = true;
+            teleportEmpty.SetActive(false);
             textMesh.text = "SMOOTH LOCOMOTION";
             LocomotionBool = true;
         }
