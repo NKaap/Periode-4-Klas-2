d5ba435e449e752043da88591d2d3278f95e0b52
NKaap <NKaap@users.noreply.github.com>
2022-06-30 23:06:06 +0200

    ScoreBoard implemented + Language changer fix 2

--- Groepje van Sjoerd 2.0/Assets/Noah/Scripts/PlayerController2.cs
+++ Groepje van Sjoerd 2.0/Assets/Noah/Scripts/PlayerController2.cs
@@ -6,6 +6,8 @@ public class PlayerController2 : MonoBehaviour
 {
     public LeaderBoard leaderboard;
 
+    public GameObject sprayGun;
+
     public int score;
 
     // Start is called before the first frame update
@@ -17,16 +19,19 @@ public class PlayerController2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            StartCoroutine(SubmitScoreRoutine());
-        }
+
     }
 
     IEnumerator SubmitScoreRoutine()
     {
-        Time.timeScale = 0f;
+        //Time.timeScale = 0f;
         yield return new WaitForSecondsRealtime(1f);
         yield return leaderboard.SubmitScoreRoutine(score);
+    }
+
+    public void SumbitScore()
+    {
+        sprayGun.GetComponent<Spray>().score = score;
+        StartCoroutine(SubmitScoreRoutine());
     }
 }
